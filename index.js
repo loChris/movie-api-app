a9261c92f6423c4cffac03f7bd37841003de6e4e
loChris <44450518+loChris@users.noreply.github.com>
2020-09-14 17:13:23 -0400

    user can now search movies via search input and results are displayed

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+	res.render('search');
+});
+
 app.get('/results', (req, res) => {
-	request(
-		'http://omdbapi.com/?s=star&apikey=thewdb',
-		(error, response, body) => {
-			if (!error && response.statusCode == 200) {
-				const data = JSON.parse(body);
-				res.render('results', { data: data });
-			}
+	const searchQuery = req.query.search;
+	const apiKey = 'apikey=thewdb';
+	const requestURL = `http://omdbapi.com/?s=${searchQuery}&${apiKey}`;
+
+	request(requestURL, (error, response, body) => {
+		if (!error && response.statusCode == 200) {
+			const data = JSON.parse(body);
+			res.render('results', { data: data });
 		}
-	);
+	});
 });
 
 app.listen(3000, () => {
