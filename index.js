d560b67c6ba66262b1574054c123a80bf03a46cf
loChris <44450518+loChris@users.noreply.github.com>
2020-09-14 16:54:01 -0400

    results now get displayed on the page in an li format

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/results', (req, res) => {
 	request(
 		'http://omdbapi.com/?s=star&apikey=thewdb',
 		(error, response, body) => {
 			if (!error && response.statusCode == 200) {
-				res.send(body);
+				const data = JSON.parse(body);
+				res.render('results', { data: data });
 			}
 		}
 	);
